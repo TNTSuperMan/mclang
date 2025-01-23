e937fab1dccf8768b2b1f02127fa400285cdb43a
TNTSuperMan <tntsuperga@gmail.com>
2025-01-23 19:42:13 +0900

    オブジェクト形式で返す

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import { setup } from "./score"
 export {addRaw} from "./base"
 export { generator as Score } from "./score"
 export { generator as Target } from "./target"
-export const OutResult = () => {
-    console.log("#Setup")
-    console.log(setup())
-    console.log("#Main")
-    console.log(command())
-}
+export const Result = () => ({
+    command: command(),
+    setup: setup()
+})
